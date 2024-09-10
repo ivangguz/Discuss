f81e8000cafcd9c9f9fa13eaeb66d0f4c30641ea
ivangguz <ivangguz@gmail.com>
2024-09-09 19:23:52 -0600

    medio terminado el header, sin tomar en cuenta el cache

---
+++ src/components/Header.tsx
@@ -0,0 +1,66 @@
+import Link from "next/link";
+import {
+    Navbar,
+    NavbarBrand,
+    NavbarContent,
+    NavbarItem,
+    Input,
+    Button,
+    Avatar,
+    Popover,
+    PopoverTrigger,
+    PopoverContent
+} from '@nextui-org/react';
+import {auth} from "@/auth";
+import * as actions from '@/actions';
+
+export default async function Header(){
+    const session = await auth();
+
+    let authContent: React.ReactNode;
+    if (session?.user){
+        authContent =  (
+        <Popover placement="left">
+            <PopoverTrigger>
+                <Avatar className="cursor-pointer" src={session.user.image || ''}/>
+            </PopoverTrigger>
+            <PopoverContent>
+                <div className="p-4">
+                    <form action={actions.signOut}>
+                        <Button type="submit">Sign Out</Button>
+                    </form>
+                </div>
+            </PopoverContent>
+        </Popover>
+        );
+    } else{
+        authContent = <>
+            <NavbarItem>
+                <form action={actions.signIn}>
+                    <Button type="submit" color="secondary" variant="bordered">Sign In</Button>
+                </form>
+            </NavbarItem>
+            <NavbarItem>
+                <form action={actions.signOut}>
+                    <Button type="submit" color="primary" variant="flat">Sign Out</Button>
+                </form>
+            </NavbarItem>
+        </>
+    }
+
+    return(
+        <Navbar className="shadow mb-6">
+            <NavbarBrand>
+                <Link href="/" className="font-bold">Discuss</Link>
+            </NavbarBrand>
+            <NavbarContent justify="center">
+                <NavbarItem>
+                    <Input/>
+                </NavbarItem>
+            </NavbarContent>
+            <NavbarContent justify="end">
+                    {authContent}
+            </NavbarContent>
+        </Navbar>
+    );
+}
